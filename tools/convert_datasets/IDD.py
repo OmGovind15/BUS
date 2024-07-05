4afed1283981f1f64810318304db4754bec92f91
Your Name <dragoon0905@naver.com>
2024-07-05 12:54:03 +0900

    origin/main

---
+++ tools/convert_datasets/IDD.py
@@ -0,0 +1,127 @@
+# Obtained from: https://github.com/lhoyer/DAFormer
+# ---------------------------------------------------------------
+# Copyright (c) 2021-2022 ETH Zurich, Lukas Hoyer. All rights reserved.
+# Licensed under the Apache License, Version 2.0
+# ---------------------------------------------------------------
+
+# Obtained from: https://github.com/open-mmlab/mmsegmentation/tree/v0.16.0
+# Modifications: Add class stats computation
+
+import argparse
+import json
+import os.path as osp
+
+import mmcv
+import numpy as np
+from cityscapesscripts.preparation.json2labelImg import json2labelImg
+from PIL import Image
+
+
+
+def convert_to_train_id(file):
+    # re-assign labels to match the format of Cityscapes
+    pil_label = Image.open(file)
+    label = np.asarray(pil_label)
+    id_to_trainid = {
+        0: 0,
+        3: 1,
+        29: 2,
+        20: 3,
+        21: 4,
+        26: 5,
+        25: 6,
+        24: 7,
+        32: 8,
+        33: 9,
+        6: 10,
+        8: 11,
+        12: 12,
+        13: 13,
+        14: 14,
+        17: 15,
+        9: 16,
+        10: 17
+    }
+    label_copy = 255 * np.ones(label.shape, dtype=np.uint8)
+    sample_class_stats = {}
+    for k, v in id_to_trainid.items():
+        k_mask = label == k
+        label_copy[k_mask] = v
+        n = int(np.sum(k_mask))
+        if n > 0:
+            sample_class_stats[v] = n
+    new_file = file.replace('gtFine_labelids.png', '_labelTrainIds18.png')
+    assert file != new_file
+    sample_class_stats['file'] = new_file
+    Image.fromarray(label_copy, mode='L').save(new_file)
+    return sample_class_stats
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='Convert IDD annotations to TrainIds')
+    parser.add_argument('idd_path', help='idd data path')
+    parser.add_argument('--gt-dir', default='gtFine', type=str)
+    parser.add_argument('-o', '--out-dir', help='output path')
+    parser.add_argument(
+        '--nproc', default=1, type=int, help='number of process')
+    args = parser.parse_args()
+    return args
+
+
+def save_class_stats(out_dir, sample_class_stats):
+    sample_class_stats = [e for e in sample_class_stats if e is not None]
+    with open(osp.join(out_dir, 'sample_class_stats.json'), 'w') as of:
+        json.dump(sample_class_stats, of, indent=2)
+
+    sample_class_stats_dict = {}
+    for stats in sample_class_stats:
+        f = stats.pop('file')
+        sample_class_stats_dict[f] = stats
+    with open(osp.join(out_dir, 'sample_class_stats_dict.json'), 'w') as of:
+        json.dump(sample_class_stats_dict, of, indent=2)
+
+    samples_with_class = {}
+    for file, stats in sample_class_stats_dict.items():
+        for c, n in stats.items():
+            if c not in samples_with_class:
+                samples_with_class[c] = [(file, n)]
+            else:
+                samples_with_class[c].append((file, n))
+    with open(osp.join(out_dir, 'samples_with_class.json'), 'w') as of:
+        json.dump(samples_with_class, of, indent=2)
+
+
+def main():
+    args = parse_args()
+    cityscapes_path = "/data/dragoon0905/datasets/IDD"#args.cityscapes_path
+    out_dir = args.out_dir if args.out_dir else cityscapes_path
+    mmcv.mkdir_or_exist(out_dir)
+
+    gt_dir = osp.join(cityscapes_path, args.gt_dir)
+
+    poly_files = []
+    for poly in mmcv.scandir(
+            gt_dir, 'gtFine_labelids.png',
+            recursive=True):
+        poly_file = osp.join(gt_dir, poly)
+        poly_files.append(poly_file)
+    poly_files = sorted(poly_files)
+
+    only_postprocessing = False
+    if not only_postprocessing:
+        if args.nproc > 1:
+            sample_class_stats = mmcv.track_parallel_progress(
+                convert_to_train_id, poly_files, args.nproc)
+        else:
+            sample_class_stats = mmcv.track_progress(convert_to_train_id,
+                                                     poly_files)
+    else:
+        with open(osp.join(out_dir, 'sample_class_stats.json'), 'r') as of:
+            sample_class_stats = json.load(of)
+
+    save_class_stats(out_dir, sample_class_stats)
+
+
+if __name__ == '__main__':
+    main()
